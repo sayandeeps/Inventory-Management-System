86e67f27b87fd28e78d1cc075dc5026f4ab0711b
Aman <amankunalbharti2003@gmail.com>
2023-12-01 00:50:13 +0530

    product details page

--- app/Views/searchproduct.php
+++ app/Views/searchproduct.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,17 +10,108 @@
 <body>
     
 <form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only  :text-white">Search</label>
     <div class="relative">
         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <svg class="w-4 h-4 text-gray-500  :text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
-        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300  bg-gray-50 focus:ring-blue-500 focus:border-blue-500  :bg-gray-700  :border-gray-600  :placeholder-gray-400  :text-white  :focus:ring-blue-500  :focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2  :bg-blue-600  :hover:bg-blue-700  :focus:ring-blue-800">Search</button>
     </div>
 </form>
+
+<section class="flex  items-center bg-gray-100  font-poppins  :bg-gray-900 ">
+        <div class="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
+            <div class="flex flex-wrap justify-center -mx-3 pt-11">
+                <div class="w-full px-3 mb-6 lg:px-2 ">
+                    <div class="bg-white rounded-md  :bg-gray-800">
+                        <div class="p-4">
+                            <div class="flex items-center justify-between mb-3">
+                                <span
+                                    class="inline-block px-2 py-1 text-sm text-blue-500 rounded-full  :bg-gray-700  :text-gray-400 bg-blue-50">
+                                    Full time
+                                </span>
+                                <h2 class="text-sm font-medium  :text-gray-400">3 days from now</h2>
+                            </div>
+                            <h2 class="mb-3 text-xl font-semibold lg:text-2xl  :text-gray-400">
+                                Vacancy- Java developer @ABC Company
+                            </h2>
+                            <div class="flex items-center justify-between mb-3">
+                                <h2 class="text-sm font-medium  :text-gray-400">Starts from</h2>
+                                <span
+                                    class="inline-block px-2 py-1 text-sm text-blue-500 rounded-full bg-blue-50  :bg-gray-700  :text-gray-400">
+                                    7th march 2022</span>
+                            </div>
+                            <p class="text-sm font-medium  :text-gray-400">Duration : <strong>2.5 months</strong></p>
+                            <div class="flex items-center justify-center mt-4">
+                                <a href=""
+                                    class="w-full px-4 py-2 text-center text-gray-900 bg-blue-300 rounded-md  :bg-gray-700  :text-gray-400  :hover:bg-gray-900 hover:bg-blue-400">
+                                    Apply now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full px-3 mb-6 lg:px-2 ">
+                    <div class="bg-white rounded-md  :bg-gray-800">
+                        <div class="p-4">
+                            <div class="flex items-center justify-between mb-3">
+                                <span
+                                    class="inline-block px-2 py-1 text-sm text-blue-500 rounded-full  :bg-gray-700  :text-gray-400 bg-blue-50">
+                                    Full time
+                                </span>
+                                <h2 class="text-sm font-medium  :text-gray-400">3 days from now</h2>
+                            </div>
+                            <h2 class="mb-3 text-xl font-semibold lg:text-2xl  :text-gray-400">
+                                Vacancy- Web developer @ABC Company
+                            </h2>
+                            <div class="flex items-center justify-between mb-3">
+                                <h2 class="text-sm font-medium  :text-gray-400">Starts from</h2>
+                                <span
+                                    class="inline-block px-2 py-1 text-sm text-blue-500 rounded-full bg-blue-50  :bg-gray-700  :text-gray-400">
+                                    7th march 2022</span>
+                            </div>
+                            <p class="text-sm font-medium  :text-gray-400">Duration : <strong>2.5 months</strong></p>
+                            <div class="flex items-center justify-center mt-4">
+                                <a href=""
+                                    class="w-full px-4 py-2 text-center text-gray-900 bg-blue-300 rounded-md  :bg-gray-700  :text-gray-400  :hover:bg-gray-900 hover:bg-blue-400">
+                                    Apply now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full px-3 mb-6 lg:px-2  ">
+                    <div class="bg-white rounded-md  :bg-gray-800">
+                        <div class="p-4">
+                            <div class="flex items-center justify-between mb-3">
+                                <span
+                                    class="inline-block px-2 py-1 text-sm text-blue-500 rounded-full  :bg-gray-700  :text-gray-400 bg-blue-50">
+                                    Full time
+                                </span>
+                                <h2 class="text-sm font-medium  :text-gray-400">3 days from now</h2>
+                            </div>
+                            <h2 class="mb-3 text-xl font-semibold lg:text-2xl  :text-gray-400">
+                                Vacancy- .Net developer @ABC Company
+                            </h2>
+                            <div class="flex items-center justify-between mb-3">
+                                <h2 class="text-sm font-medium  :text-gray-400">Starts from</h2>
+                                <span
+                                    class="inline-block px-2 py-1 text-sm text-blue-500 rounded-full bg-blue-50  :bg-gray-700  :text-gray-400">
+                                    7th march 2022</span>
+                            </div>
+                            <p class="text-sm font-medium  :text-gray-400">Duration : <strong>2.5 months</strong></p>
+                            <div class="flex items-center justify-center mt-4">
+                                <a href=""
+                                    class="w-full px-4 py-2 text-center text-gray-900 bg-blue-300 rounded-md  :bg-gray-700  :text-gray-400  :hover:bg-gray-900 hover:bg-blue-400">
+                                    Apply now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </body>
 </html>
