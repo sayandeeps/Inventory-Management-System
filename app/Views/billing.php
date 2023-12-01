6633511af2db6ca668e20607a52451f3862ac7f3
Aman <amankunalbharti2003@gmail.com>
2023-12-01 16:52:05 +0530

    billing page

--- app/Views/billing.php
+++ app/Views/billing.php
@@ -8,8 +8,8 @@
 
 </head>
 <body>
-    <div class="grid grid-cols-2 gap-2">
-        <div class=" mb-4 ">
+    <div class="flex flex-wrap ">
+        <div class=" mb-4 w-full md:w-1/2 class1 ">
             <div class="relative font-poppins">
                 <div class="fixed inset-0 bg-gray-700 opacity-25"></div>
                 <div class="fixed top-0 bottom-0 z-10 w-full max-w-xl overflow-y-scroll bg-white  :bg-gray-800">
@@ -68,123 +68,124 @@
             </div>
         </div>
 
-        <div class="  mb-4  ">
-            <section class="flex  right-0 items-center py-16 bg-gray-50 font-poppins       :bg-gray-800 ">
-                <div
-                    class="justify-center flex-1 max-w-4xl px-6 py-6 mx-auto bg-gray-100 rounded-md shadow-md       :border-gray-900       :bg-gray-900 lg:py-10 lg:px-10">
-                    <div class="mb-16 text-center">
-                        <h1
-                            class="mb-6 text-2xl font-semibold leading-7 tracking-wide text-gray-700 lg:text-4xl       :text-gray-300 lg:leading-9">
-                            Thank you for your Order, John!</h1>
-                        <p class="text-lg text-gray-500       :text-gray-400">your order number is: 012736373737</p>
-                    </div>
-                    <div class="max-w-4xl mx-auto mb-10">
-                        <h2 class="mb-4 text-xl font-medium       :text-gray-400">What you ordered:</h2>
-                        <div
-                            class="p-10 mb-8 bg-white rounded-md shadow       :bg-gray-800 sm:flex sm:items-center xl:py-5 xl:px-12">
-                            <a href="#" class="mr-6 md:mr-12">
-                                <img class=" w-full lg:w-[80px] h-[200px] lg:h-[80px]  object-cover  mx-auto mb-6 sm:mb-0 "
-                                    src="https://i.postimg.cc/YS2pPP8k/white-denim-jacket-front-view-streetwear-fashion.jpg "
-                                    alt="dress">
-                            </a>
-                            <div>
-                                <a class="inline-block mb-1 text-lg font-medium hover:underline       :text-gray-400" href="#">
-                                    White jacket for men</a>
-                                <div class="flex flex-wrap">
-                                    <p class="mr-4 text-sm font-medium">
-                                        <span class="font-medium       :text-gray-400">Color:</span>
-                                        <span class="ml-2 text-gray-400       :text-gray-400">Silver</span>
-                                    </p>
-                                    <p class="mr-4 text-sm font-medium">
-                                        <span class="font-medium       :text-gray-400">Size:</span>
-                                        <span class="ml-2 text-gray-400       :text-gray-400">medium</span>
-                                    </p>
-                                    <p class="mr-4 text-sm font-medium">
-                                        <span class="font-medium       :text-gray-400">Style:</span>
-                                        <span class="ml-2 text-gray-400       :text-gray-400">Uk minimal design</span>
-                                    </p>
-                                    <p class="text-sm font-medium       :text-gray-400">
-                                        <span>Qty:</span>
-                                        <span class="ml-2 text-gray-400">1</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-10 bg-white rounded-md shadow       :bg-gray-800 sm:flex sm:items-center xl:py-5 xl:px-12">
-                            <a href="#" class="mr-6 md:mr-12">
-                                <img class=" w-full lg:w-[80px] h-[200px] lg:h-[80px] object-cover  mx-auto mb-6 sm:mb-0 "
-                                    src="https://i.postimg.cc/br9C4mmc/430.jpg " alt="dress">
-                            </a>
-                            <div>
-                                <a class="inline-block mb-1 text-lg font-medium       :text-gray-400 hover:underline" href="#">
-                                    Summer Black T-shirt
-                                </a>
-                                <div class="flex flex-wrap">
-                                    <p class="mr-4 text-sm font-medium       :text-gray-400">
-                                        <span class="font-medium">Color:</span>
-                                        <span class="ml-2 text-gray-400">Silver</span>
-                                    </p>
-                                    <p class="mr-4 text-sm font-medium       :text-gray-400">
-                                        <span class="font-medium">Size:</span>
-                                        <span class="ml-2 text-gray-400">medium</span>
-                                    </p>
-                                    <p class="mr-4 text-sm font-medium       :text-gray-400">
-                                        <span class="font-medium">Style:</span>
-                                        <span class="ml-2 text-gray-400">Uk minimal design</span>
-                                    </p>
-                                    <p class="text-sm font-medium       :text-gray-400">
-                                        <span>Qty:</span>
-                                        <span class="ml-2 text-gray-400">1</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="max-w-4xl mx-auto ">
-                        <h2 class="mb-4 text-xl font-medium       :text-gray-400 ">Order Details:</h2>
-                        <div class="grid grid-cols-1 gap-8 mb-10 lg:grid-cols-3">
-                            <div
-                                class="flex items-center justify-between px-10 py-3 font-medium leading-8 bg-white bg-opacity-50 rounded-md shadow       :text-gray-400       :bg-gray-800 font-heading">
-                                <span>Shipping</span>
-                                <span class="flex items-center">
-                                    <span class="ml-3 mr-1 text-sm">Rs.</span>
-                                    <span class="text-xl">4,000</span>
-                                </span>
-                            </div>
-                            <div
-                                class="relative flex items-center justify-between px-10 py-3 font-medium leading-8 bg-white bg-opacity-50 rounded-md shadow       :text-gray-400       :bg-gray-800">
-                                <div
-                                    class="absolute right-0 flex items-center justify-center bg-blue-500 rounded-md w-14 h-14       :bg-gray-600">
-                                    <div
-                                        class="flex items-center justify-center text-lg font-bold text-blue-500 bg-gray-100 rounded-full       :text-gray-300       :bg-gray-700 w-11 h-11">
-                                        2</div>
-                                </div>
-                                <span class="mr-16">Products</span>
-                            </div>
-                            <div
-                                class="flex items-center justify-between px-10 py-3 font-medium leading-8 bg-white rounded-md shadow       :text-gray-400       :bg-gray-800 font-heading">
-                                <span>Total</span>
-                                <span class="flex items-center text-blue-500       :text-blue-400">
-                                    <span class="ml-3 mr-1 text-sm">Rs.</span>
-                                    <span class="text-xl">7,000</span>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap items-center justify-between gap-4 ">
-                            <button
-                                class="w-full px-6 py-3 text-blue-500 border border-blue-500 rounded-md md:w-auto hover:text-gray-100 hover:bg-blue-600       :border-gray-800       :hover:bg-gray-800       :text-gray-300">
-                                Go back shopping
-                            </button>
-                            <button
-                                class="w-full px-6 py-3 text-gray-100 bg-blue-500 rounded-md md:w-auto       :text-gray-300 hover:bg-blue-600       :hover:bg-gray-800       :bg-gray-700">
-                                Download Invoice
-                            </button>
-                        </div>
-                    </div>
-        
+        <div class=" mb-4 w-full md:w-1/2 class2 bg-white md:bg-gray-800">
+        <section class="flex items-center  bg-gray-50 font-poppins   :bg-gray-800 ">
+            <div
+                class="justify-center flex-1 max-w-4xl px-6 py-6 mx-auto bg-gray-100 rounded-md shadow-md   :border-gray-900   :bg-gray-900 lg:py-10 lg:px-10">
+                <div class="mb-16 text-center">
+                    <h1
+                        class="mb-6 text-2xl font-semibold leading-7 tracking-wide text-gray-700 lg:text-4xl   :text-gray-300 lg:leading-9">
+                        Thank you for your Order, John!</h1>
+                    <p class="text-lg text-gray-500   :text-gray-400">your order number is: 012736373737</p>
                 </div>
-            </section>
+                <div class="max-w-4xl mx-auto mb-10">
+                    <h2 class="mb-4 text-xl font-medium   :text-gray-400">What you ordered:</h2>
+                    <div
+                        class="p-10 mb-8 bg-white rounded-md shadow   :bg-gray-800 sm:flex sm:items-center xl:py-5 xl:px-12">
+                        <a href="#" class="mr-6 md:mr-12">
+                            <img class=" w-full lg:w-[80px] h-[200px] lg:h-[80px]  object-cover  mx-auto mb-6 sm:mb-0 "
+                                src="https://i.postimg.cc/YS2pPP8k/white-denim-jacket-front-view-streetwear-fashion.jpg "
+                                alt="dress">
+                        </a>
+                        <div>
+                            <a class="inline-block mb-1 text-lg font-medium hover:underline   :text-gray-400" href="#">
+                                White jacket for men</a>
+                            <div class="flex flex-wrap">
+                                <p class="mr-4 text-sm font-medium">
+                                    <span class="font-medium   :text-gray-400">Color:</span>
+                                    <span class="ml-2 text-gray-400   :text-gray-400">Silver</span>
+                                </p>
+                                <p class="mr-4 text-sm font-medium">
+                                    <span class="font-medium   :text-gray-400">Size:</span>
+                                    <span class="ml-2 text-gray-400   :text-gray-400">medium</span>
+                                </p>
+                                <p class="mr-4 text-sm font-medium">
+                                    <span class="font-medium   :text-gray-400">Style:</span>
+                                    <span class="ml-2 text-gray-400   :text-gray-400">Uk minimal design</span>
+                                </p>
+                                <p class="text-sm font-medium   :text-gray-400">
+                                    <span>Qty:</span>
+                                    <span class="ml-2 text-gray-400">1</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-10 bg-white rounded-md shadow   :bg-gray-800 sm:flex sm:items-center xl:py-5 xl:px-12">
+                        <a href="#" class="mr-6 md:mr-12">
+                            <img class=" w-full lg:w-[80px] h-[200px] lg:h-[80px] object-cover  mx-auto mb-6 sm:mb-0 "
+                                src="https://i.postimg.cc/br9C4mmc/430.jpg " alt="dress">
+                        </a>
+                        <div>
+                            <a class="inline-block mb-1 text-lg font-medium   :text-gray-400 hover:underline" href="#">
+                                Summer Black T-shirt
+                            </a>
+                            <div class="flex flex-wrap">
+                                <p class="mr-4 text-sm font-medium   :text-gray-400">
+                                    <span class="font-medium">Color:</span>
+                                    <span class="ml-2 text-gray-400">Silver</span>
+                                </p>
+                                <p class="mr-4 text-sm font-medium   :text-gray-400">
+                                    <span class="font-medium">Size:</span>
+                                    <span class="ml-2 text-gray-400">medium</span>
+                                </p>
+                                <p class="mr-4 text-sm font-medium   :text-gray-400">
+                                    <span class="font-medium">Style:</span>
+                                    <span class="ml-2 text-gray-400">Uk minimal design</span>
+                                </p>
+                                <p class="text-sm font-medium   :text-gray-400">
+                                    <span>Qty:</span>
+                                    <span class="ml-2 text-gray-400">1</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="max-w-4xl mx-auto ">
+                    <h2 class="mb-4 text-xl font-medium   :text-gray-400 ">Order Details:</h2>
+                    <div class="grid grid-cols-1 gap-5 mb-10 lg:grid-cols-1">
+                        <div
+                            class="flex items-center justify-between px-10 py-3 font-medium leading-8 bg-white bg-opacity-50 rounded-md shadow   :text-gray-400   :bg-gray-800 font-heading">
+                            <span>Shipping</span>
+                            <span class="flex items-center">
+                                <span class="ml-3 mr-1 text-sm">Rs.</span>
+                                <span class="text-xl">4,000</span>
+                            </span>
+                        </div>
+                        <div
+                            class="relative flex items-center justify-between px-10 py-3 font-medium leading-8 bg-white bg-opacity-50 rounded-md shadow   :text-gray-400   :bg-gray-800">
+                            <div
+                                class="absolute right-0 flex items-center justify-center bg-blue-500 rounded-md w-14 h-14   :bg-gray-600">
+                                <div
+                                    class="flex items-center justify-center text-lg font-bold text-blue-500 bg-gray-100 rounded-full   :text-gray-300   :bg-gray-700 w-11 h-11">
+                                    2</div>
+                            </div>
+                            <span class="mr-16">Products</span>
+                        </div>
+                        <div
+                            class="flex items-center justify-between px-10 py-3 font-medium leading-8 bg-white rounded-md shadow   :text-gray-400   :bg-gray-800 font-heading">
+                            <span>Total</span>
+                            <span class="flex items-center text-blue-500   :text-blue-400">
+                                <span class="ml-3 mr-1 text-sm">Rs.</span>
+                                <span class="text-xl">7,000</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap items-center justify-between gap-4 ">
+                        <button
+                            class="w-full px-6 py-3 text-blue-500 border border-blue-500 rounded-md md:w-auto hover:text-gray-100 hover:bg-blue-600   :border-gray-800   :hover:bg-gray-800   :text-gray-300">
+                            Go back shopping
+                        </button>
+                        <button
+                            class="w-full px-6 py-3 text-gray-100 bg-blue-500 rounded-md md:w-auto   :text-gray-300 hover:bg-blue-600   :hover:bg-gray-800   :bg-gray-700">
+                            Download Invoice
+                        </button>
+                    </div>
+                </div>
+    
+            </div>
+        </section>
         </div>
+        
     </div>
 
 
