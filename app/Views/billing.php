666180da31d206d172146b8e41c9e92273d423df
Aman <amankunalbharti2003@gmail.com>
2023-12-01 13:07:18 +0530

    add billing

---
+++ app/Views/billing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+<div class="flex flex-wrap -mb-4">
+  <div class="w-1/2 mb-4 bg-gray-400 h-12"></div>
+  <div class="w-1/2 mb-4 bg-gray-500 h-12"></div>
+  
+</div>
+</body>
+</html>
